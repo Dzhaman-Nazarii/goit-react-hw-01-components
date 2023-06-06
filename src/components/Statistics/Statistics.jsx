b8cd313b2d3d7914cfad4ab69ddeb5c8c1cbc4e7
Dzhaman-Nazarii <dzhaman.nazar2003@gmail.com>
2023-06-06 11:26:55 +0300

    exercise 2

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types'
+import css from "./Statistics.module.css";
+
+const Statistics = ({ title, stats }) => {
+    return (
+        <section className={css.statistics}>
+            {title && <h2 className={css.title}>{title}</h2>}
+            <ul className={css.statList}>
+                {stats.map(({ id, label, percentage }) => {
+                    return (
+                        <li className={css.stats}
+                        key={id}>
+                            <span>{label}</span>
+                            <span>{percentage}%</span>
+                        </li>
+                    )
+                })}
+
+            </ul>
+        </section>
+    )
+}
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.array.isRequired,
+
+}
+
+export default Statistics;
